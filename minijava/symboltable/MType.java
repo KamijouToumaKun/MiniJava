ee557dfddbb80480352ea5c8aa72f6d76cc5ba20
KamijouToumaKun <36612947+KamijouToumaKun@users.noreply.github.com>
2018-03-28 01:01:55 +0800

    Add files via upload

--- minijava/symboltable/MType.java
+++ minijava/symboltable/MType.java
@@ -5,6 +5,49 @@ public class MType {
     protected int line = 0;
     protected int column = 0;
 
+    protected int hasInitLength = 0;
+    // 0: has initialized, but not sure of value
+    // -1: has not initialized
+    // 1: has initialized, and sure of value
+    protected int length;
+    public int getHasInitLength() {
+        return hasInitLength;
+    }
+    public void setHasInitLength(int hasInitLength) {
+        this.hasInitLength = hasInitLength;
+    }
+    public int getLength() {
+        return length;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    protected int hasInit = 0;
+    // 0: has initialized, but not sure of value
+    // -1: has not initialized
+    // 1: has initialized, and sure of value
+    protected int intValue;
+    protected boolean booleanValue;
+    public int getHasInit() {
+        return hasInit;
+    }
+    public void setHasInit(int hasInit) {
+        this.hasInit = hasInit;
+    }
+    public void setIntValue(int intValue) {
+        this.intValue = intValue;
+    }
+    public void setBooleanValue(boolean booleanValue) {
+        this.booleanValue = booleanValue;
+    }
+    public int getIntValue() {
+        return intValue;
+    }
+    public boolean getBooleanValue() {
+        return booleanValue;
+    }
+
     public MType() {
         
     }
@@ -21,7 +64,6 @@ public class MType {
     public int hashCode() {  
         return name.hashCode();  
     }  
-
 
     public MType(String name, int line, int column) {
         this.name = name;
