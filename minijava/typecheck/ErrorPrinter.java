9312d7892b6a3eb3157fee8c34c2e16c5e3a6090
KamijouToumaKun <36612947+KamijouToumaKun@users.noreply.github.com>
2018-04-06 13:10:19 +0800

    Add files via upload

--- minijava/typecheck/ErrorPrinter.java
+++ minijava/typecheck/ErrorPrinter.java
@@ -8,6 +8,9 @@ public class ErrorPrinter {
         System.out.println("line:" + line + " column:" + column + '\n' + content);
         size ++;
     }
+    public void printError(int line,int column,String content, int flag) {
+        System.out.println("WARNING:\n" + "line:" + line + " column:" + column + '\n' + content + '\n');
+    }
     public static int getSize() {
     	return size;
     }
