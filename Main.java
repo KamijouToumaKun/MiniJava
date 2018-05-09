c6be0a23ea1f374b1b2633e6e49dd64992551a02
KamijouToumaKun <36612947+KamijouToumaKun@users.noreply.github.com>
2018-05-09 17:31:22 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,65 +1,88 @@
-import minijava.typecheck.ErrorPrinter;
-
 import minijava.MiniJavaParser;
 import minijava.ParseException;
-import minijava.TokenMgrError; 
+import minijava.TokenMgrError;
+import minijava.symboltable.MClass;
+import minijava.symboltable.MClassList;
+import minijava.symboltable.MPiglet;
+import minijava.syntaxtree.Node;
+import minijava.visitor.GJDepthFirst;
 
-import minijava.syntaxtree.Node; 
-import minijava.visitor.SymbolTableVisitor; 
+import minijava.typecheck.ErrorPrinter;
+
+import minijava.visitor.SymbolTableVisitor;
 import minijava.visitor.TypeCheckVisitor;
+import minijava.visitor.MinijavaToPigletVisitor;
 
-import minijava.symboltable.MClass; 
-import minijava.symboltable.MMethod; 
-import minijava.symboltable.MVar; 
-import minijava.symboltable.MType; 
-import minijava.symboltable.MClassList; 
-import minijava.symboltable.MCallList; 
+import java.io.*;
+import java.util.Scanner;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+public class Main {
+	public static void main(String[] args) {
+		try {
+			// InputStream is = new FileInputStream("test/BinaryTree.java");
+			// InputStream is = new FileInputStream("test/BubbleSort.java");
+			// InputStream is = new FileInputStream("test/Factorial.java");
+			// InputStream is = new FileInputStream("test/LinearSearch.java");
+			// InputStream is = new FileInputStream("test/LinkedList.java");
+			// InputStream is = new FileInputStream("test/MoreThan4.java");
+			// InputStream is = new FileInputStream("test/QuickSort.java");
+			InputStream is = new FileInputStream("test/TreeVisitor.java");
+			// InputStream is = new FileInputStream("test/IndexRange.java");
+			// InputStream is = new FileInputStream("test/Initialization.java");
 
-
-public class Main { 
- 
-    public static void main(String[] args) {
-    	try {
-			// InputStream is = new FileInputStream("test/BinaryTree-error.java");
-            // InputStream is = new FileInputStream("test/BinaryTree.java");
-            // InputStream is = new FileInputStream("test/BubbleSort-error.java");
-            InputStream is = new FileInputStream("test/BubbleSort.java");
-            // InputStream is = new FileInputStream("test/Factorial-error.java");
-            // InputStream is = new FileInputStream("test/Factorial.java");
-            // InputStream is = new FileInputStream("test/LinearSearch-error.java");
-            // InputStream is = new FileInputStream("test/LinearSearch.java");
-            // InputStream is = new FileInputStream("test/LinkedList-error.java");
-            // InputStream is = new FileInputStream("test/LinkedList.java");
-            // InputStream is = new FileInputStream("test/MoreThan4-error.java");
-            // InputStream is = new FileInputStream("test/MoreThan4.java");
-            // InputStream is = new FileInputStream("test/QuickSort-error.java");
-            // InputStream is = new FileInputStream("test/QuickSort.java");
-            // InputStream is = new FileInputStream("test/TreeVisitor-error.java");
-            // InputStream is = new FileInputStream("test/TreeVisitor.java");
-            // InputStream is = new FileInputStream("test/IndexRange-error.java");
-            // InputStream is = new FileInputStream("test/IndexRange.java");
-            // InputStream is = new FileInputStream("test/Initialization-error.java");
-            // InputStream is = new FileInputStream("test/Initialization.java");
+			// InputStream is = new FileInputStream("test/ClassAsParam.java");
+			// InputStream is = new FileInputStream("test/TooManyParams.java");
 			
-            Node root = new MiniJavaParser(is).Goal();
-			root.accept(new SymbolTableVisitor(),MClassList.instance);
-			root.accept(new TypeCheckVisitor(), MClassList.instance);
-			if (ErrorPrinter.getSize() > 0) {
-                System.out.println("Type error");
-            } else {
-                System.out.println("Program type checked successfully");
-            }
-		} catch (TokenMgrError e){
-    		e.printStackTrace();
-    	} catch (ParseException e){
-    		e.printStackTrace();
-    	} catch (Exception e){
-    		e.printStackTrace();
-    	}
-    }
+			// InputStream is = new FileInputStream("test/AnimalDog/AD.java"); 
+			// 20
+			// InputStream is = new FileInputStream("test/AnimalDog/2Age.java"); 
+			// 0
+			// InputStream is = new FileInputStream("test/AnimalDog/UselessExtend.java");
+			// 10
+			// InputStream is = new FileInputStream("test/AnimalDog/jyz.java");
+			// 0
+			// InputStream is = new FileInputStream("test/AnimalDog/AllFromParent.java");
+			// 10
+			
+			PrintStream out = new PrintStream("mine.pg");
+			String code = "";
+			Scanner sc = new Scanner(is);
+			while (sc.hasNext()) {
+				code += sc.nextLine() + "\n";
+			}
+			out.println(mini2pig(code));
+			/*
+			 * TODO: Implement your own Visitors and other classes.
+			 * 
+			 */
+		}
+		catch(TokenMgrError e){
+			//Handle Lexical Errors
+			e.printStackTrace();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public static String mini2pig(String s) {
+		InputStream in = new ByteArrayInputStream(s.getBytes());
+		Node root = new minijava.syntaxtree.NodeToken("rr");
+		try {
+			root = new MiniJavaParser(in).Goal();
+		} catch (minijava.ParseException e) {
+			e.printStackTrace();
+			return "ERROR";
+		}
+		root.accept(new SymbolTableVisitor(), MClassList.instance);
+		root.accept(new TypeCheckVisitor(), MClassList.instance);
+		if (ErrorPrinter.getSize() > 0) {
+			System.out.println("Type Error");
+			return "ERROR";
+		}
+		MClassList.instance.completeClass();
+		MClassList.instance.allocTemp(20);
+		MPiglet ans = root.accept(new MinijavaToPigletVisitor(), MClassList.instance);
+		return ans.getCode().toString();
+	}
 }
