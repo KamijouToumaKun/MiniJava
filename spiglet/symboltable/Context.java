59edecd8889bc6d2530afd6389ac23d6a131e928
KamijouToumaKun <36612947+KamijouToumaKun@users.noreply.github.com>
2018-06-01 11:06:25 +0800

    Add files via upload

--- spiglet/symboltable/Context.java
+++ spiglet/symboltable/Context.java
@@ -6,11 +6,17 @@ import java.io.PrintStream;
 public class Context {
 	public static HashMap<String, Method> mMethod;
     public static HashMap<String, Integer> mLabel;
+
+    public static HashMap<String, Integer> mGlobalLabel; //methodName_label -> globalLabelNo
+    public static int globalLabelNo;
+    
     public static PrintStream out;
 
     public Context(PrintStream out) {
     	mMethod = new HashMap<String, Method>();
     	mLabel = new HashMap<String, Integer>();
+    	mGlobalLabel = new HashMap<String, Integer>();
+        globalLabelNo = 2;
     	this.out = out;
     }
 }
